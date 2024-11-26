d77b4d1d59244b65cf5c8d300ed93691eadfcc10
Emoncr <biplobemon75945@gmail.com>
2024-11-27 00:59:54 +0600

    Fix: Implemented challenges

--- src/views/userAgent/userAgent.tsx
+++ src/views/userAgent/userAgent.tsx
@@ -3,22 +3,29 @@
 import { BackToHome } from "@/components/backToHome/backToHome";
 import { useUserAgentContext } from "@/components/providers/userAgentProvider";
 
-export const UserAgent = () => {
-  const { userAgent } = useUserAgentContext();
+type UserAgentProps = {
+  userAgentData: string;
+};
+
+export const UserAgent = ({ userAgentData }: UserAgentProps) => {
+  // const { userAgent } = useUserAgentContext();
 
   return (
     <div>
       <BackToHome />
 
-      {userAgent && (
+      {/* showing user agent from server side headers */}
+      {userAgentData && (
         <div className="flex font-mono font-semibold text-sm">
           <div className="border p-2">UserAgent</div>
 
-          <div className="border p-2">{userAgent}</div>
+          <div className="border p-2">{userAgentData}</div>
         </div>
       )}
 
-      {!userAgent && <div>No user agent</div>}
+      {!userAgentData && <div>No user agent</div>}
     </div>
   );
 };
+
+export default UserAgent;
